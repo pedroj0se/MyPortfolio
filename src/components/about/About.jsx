ebb9b0018312b3fbe99a12c0a90eeb35418c2f07
pedroj0se <112190514+pedroj0se@users.noreply.github.com>
2023-07-10 12:06:54 -0500

    Subida de proyecto

---
+++ src/components/about/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './about.css';
+import AboutImg from '../../assets/about.jpg';
+import Info from './Info';
+
+const About = () => {
+
+    return (
+        <section className="about section" id="about">
+            <h2 className="section__title">About We</h2>
+            <span className="section__subtitle">Our Introduction</span>
+
+            <div className="about__container container grid">
+                <img src={AboutImg} alt="AboutImg" className="about__img" />
+
+                <div className="about__data">
+                    <Info />
+
+                    <p className="about__description">Frontend developer, I create web pages with UI / UX user interface, I have years of experience and many clients are happy with the projects carried out.</p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default About;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import './home.css';
+
+const Social = () => {
+ 
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/dev__works/" className="home__social-icon" target="_blank">
+                <i class="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.facebook.com/profile.php?id=100094286504093" className="home__social-icon" target="_blank">
+                <i class="uil uil-facebook"></i>
+            </a>
+
+            <a href="https://github.com/" className="home__social-icon" target="_blank">
+                <i class="uil uil-github"></i>
+            </a>
+        </div>
+    );
+}
+
+export default Social;
