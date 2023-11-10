0a4b2ff07bdc0e66bc434ebbc238305727d88772
Pedro Jose <112190514+pedroj0se@users.noreply.github.com>
2023-11-09 23:29:15 -0500

    services

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,19 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './home.css';
 
 const Social = () => {
  
     return (
         <div className="home__social">
-            <a href="https://www.instagram.com/dev__works/" className="home__social-icon" target="_blank">
+            <a href="https://www.instagram.com/dev__works/" className="home__social-icon" >
                 <i class="uil uil-instagram"></i>
             </a>
 
-            <a href="https://www.facebook.com/profile.php?id=100094286504093" className="home__social-icon" target="_blank">
+            <a href="https://www.facebook.com/profile.php?id=100094286504093" className="home__social-icon" >
                 <i class="uil uil-facebook"></i>
             </a>
 
-            <a href="https://github.com/" className="home__social-icon" target="_blank">
+            <a href="https://github.com/" className="home__social-icon" >
                 <i class="uil uil-github"></i>
             </a>
         </div>
