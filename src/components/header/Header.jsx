ca92627aee455a68f682972270903c76412ba04f
Pedro Jose <112190514+pedroj0se@users.noreply.github.com>
2023-11-12 17:52:32 -0500

    .

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
     return (
         <header className="header">
             <nav className="nav container">
-                <a href="index.html" className="nav__logo">DevWorks</a>
+                <a href="/" className="nav__logo">DevWorks</a>
 
                 <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                     <ul className="nav__list grid">
